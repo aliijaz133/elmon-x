e0e71c0c07f214ea6f3b6c882620f95da827561c
aliijaz133 <aliejaz0072@gmail.com>
2023-12-12 22:47:52 +0500

    UI Complete Updating

--- src/app/edit-dialog/edit-dialog.component.ts
+++ src/app/edit-dialog/edit-dialog.component.ts
@@ -8,17 +8,25 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class EditDialogComponent {
   editedPrice: number;
+
   priceUpdated = new EventEmitter<number>();
 
   constructor(
     public dialogRef: MatDialogRef<EditDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public price: number
   ) {
-    this.editedPrice = price; // Initialize editedPrice with the current price
+    this.editedPrice = price;
+  }
+
+  onInputChange(event: any): void {
+    let inputValue: string = event.target.value;
+    inputValue = inputValue.replace(/[^0-9]/g, '');
+
+    this.editedPrice = inputValue ? parseInt(inputValue, 10) : 10;
   }
 
   onSaveClick(): void {
-    this.priceUpdated.emit(this.editedPrice); // Emit the updated price
+    this.priceUpdated.emit(this.editedPrice);
     this.dialogRef.close();
   }
 }
