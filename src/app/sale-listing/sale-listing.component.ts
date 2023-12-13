b10bb32f0427d054646d2d32c89f90fd61c92442
aliijaz133 <aliejaz0072@gmail.com>
2023-12-13 14:23:07 +0500

    Edit Dialog Updating

--- src/app/sale-listing/sale-listing.component.ts
+++ src/app/sale-listing/sale-listing.component.ts
@@ -41,79 +41,79 @@ export class SaleListingComponent implements OnInit {
       },
       {
         image_url: '../../assets/images/rectangle-982-xz9.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 45,
         trend_Id: '#5683',
       },
       {
         image_url: '../../assets/images/rectangle-1076.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 55,
         trend_Id: '#5684',
       },
       {
         image_url: '../../assets/images/rectangle-984.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 65,
         trend_Id: '#5685',
       },
       {
         image_url: '../../assets/images/rectangle-985.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 75,
         trend_Id: '#5686',
       },
       {
         image_url: '../../assets/images/rectangle-1058-gbw.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 85,
         trend_Id: '#5687',
       },
       {
         image_url: '../../assets/images/rectangle-987.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 95,
         trend_Id: '#5688',
       },
       {
         image_url: '../../assets/images/rectangle-988.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 105,
         trend_Id: '#5689',
       },
       {
         image_url: '../../assets/images/rectangle-989.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 115,
         trend_Id: '#5690',
       },
       {
         image_url: '../../assets/images/rectangle-991.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 125,
         trend_Id: '#5691',
       },
       {
         image_url: '../../assets/images/rectangle-992.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 135,
         trend_Id: '#5692',
       },
       {
         image_url: '../../assets/images/rectangle-993.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 145,
         trend_Id: '#5693',
       },
       {
         image_url: '../../assets/images/rectangle-994.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 155,
         trend_Id: '#5694',
       },
       {
         image_url: '../../assets/images/rectangle-1035.png',
-        image_title: '‘Look Mum, My Painting Got Into Art Basel!’',
+        image_title: 'Look Mum, My Painting Got Into Art Basel!',
         price: 165,
         trend_Id: '#5695',
       },
@@ -142,6 +142,7 @@ export class SaleListingComponent implements OnInit {
     const dialogRef: MatDialogRef<EditDialogComponent, number> =
       this.dialog.open(EditDialogComponent, {
         data: trendItem.price,
+        disableClose: true,
       });
 
     dialogRef.componentInstance.priceUpdated.subscribe(
