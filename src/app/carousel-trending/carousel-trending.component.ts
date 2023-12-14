27320045940ac7de03b50d539789a997c6400d6c
aliijaz133 <aliejaz0072@gmail.com>
2023-12-14 17:41:14 +0500

    UI Updating:

--- src/app/carousel-trending/carousel-trending.component.ts
+++ src/app/carousel-trending/carousel-trending.component.ts
@@ -1,32 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-carousel-trending',
   templateUrl: './carousel-trending.component.html',
-  styleUrls: ['./carousel-trending.component.scss']
+  styleUrls: ['./carousel-trending.component.scss'],
 })
 export class CarouselTrendingComponent {
+  displayedCards: any[] = [];
+  currentIndex = 0;
+
+  constructor() {
+    this.updateDisplayedCards();
+  }
+
   cards = [
-    { title: '‘Look Mum, My Painting Got Into..', imageUrl: '../../assets/images/trending1.png', price: '$ 25' },
-    { title: 'Dare To Dream', imageUrl: '../../assets/images/trending2.png', price: '$ 25' },
-    { title: 'Bedlam Insanity', imageUrl: '../../assets/images/trending3.png', price: '$ 25' },
-    { title: 'Look Mum, My Painting Got Into..', imageUrl: '../../assets/images/trending4.png', price: '$ 25' },
-    { title: 'Dare To Dream', imageUrl: '../../assets/images/trending2.png', price: '$ 25' },
-    { title: 'Look Mum, My Painting Got Into..', imageUrl: '../../assets/images/trending4.png', price: '$ 25' },
-    { title: 'Bedlam Insanity', imageUrl: '../../assets/images/trending3.png', price: '$ 25' },
-    { title: '‘Look Mum, My Painting Got Into..', imageUrl: '../../assets/images/trending1.png', price: '$ 25' },
+    {
+      title: '‘Look Mum, My Painting Got Into..',
+      imageUrl: '../../assets/images/trending1.png',
+      price: '$ 25',
+    },
+    {
+      title: 'Dare To Dream',
+      imageUrl: '../../assets/images/trending2.png',
+      price: '$ 25',
+    },
+    {
+      title: 'Bedlam Insanity',
+      imageUrl: '../../assets/images/trending3.png',
+      price: '$ 25',
+    },
+    {
+      title: 'Look Mum, My Painting Got Into..',
+      imageUrl: '../../assets/images/trending4.png',
+      price: '$ 25',
+    },
+    {
+      title: 'Dare To Dream',
+      imageUrl: '../../assets/images/trending2.png',
+      price: '$ 25',
+    },
+    {
+      title: 'Look Mum, My Painting Got Into..',
+      imageUrl: '../../assets/images/trending4.png',
+      price: '$ 25',
+    },
+    {
+      title: 'Bedlam Insanity',
+      imageUrl: '../../assets/images/trending3.png',
+      price: '$ 25',
+    },
+    {
+      title: '‘Look Mum, My Painting Got Into..',
+      imageUrl: '../../assets/images/trending1.png',
+      price: '$ 25',
+    },
   ];
-  displayedCards() {
-    if (window.innerWidth < 992) {
-      return this.cards.slice(0, 2); // Display 2 cards for smaller screens
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateDisplayedCards();
+  }
+
+  private updateDisplayedCards() {
+    if (window.innerWidth < 769) {
+      this.displayedCards = [this.cards[this.currentIndex]];
+    } else if (window.innerWidth < 992) {
+      this.displayedCards = this.cards.slice(
+        this.currentIndex,
+        this.currentIndex + 2
+      );
     } else {
-      return this.cards.slice(0, 4); // Display 4 cards for larger screens
+      this.displayedCards = this.cards.slice(
+        this.currentIndex,
+        this.currentIndex + 4
+      );
     }
   }
 
-  constructor() {
-    window.addEventListener('resize', () => {
-      this.displayedCards(); // Update displayed cards when the window size changes
-    });
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.cards.length;
+    this.updateDisplayedCards();
+  }
+
+  previous() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.cards.length) % this.cards.length;
+    this.updateDisplayedCards();
   }
 }
